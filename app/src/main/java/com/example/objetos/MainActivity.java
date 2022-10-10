910ffe7dbb91b909800fcd957bf9dfbdb9a9ba38
Edgar D; Caamal <caamal.edgar@gmail.com>
2022-10-09 21:49:41 -0500

    Objeto Final

--- app/src/main/java/com/example/objetos/MainActivity.java
+++ app/src/main/java/com/example/objetos/MainActivity.java
@@ -15,6 +15,12 @@ import com.example.objetos.Clases.Lavadora;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    //TextView textView;
+    //Lavadora lavadora;
+    //Lavadora lavadoraSecadora;
+
+    //=  (TextView) findViewById(R.id.tvMensaje);
+//    Lavadora lavadora   = new Lavadora();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +29,37 @@ public class MainActivity extends AppCompatActivity {
 
         //Creando una instancia de nuestro objeto
         Lavadora lavadora   = new Lavadora();
-
         lavadora.setMarca("Mabe");
         lavadora.setModelo("Lavado Autómatico");
+        Log.d("Objeto lavadora", lavadora.toString());
 
-        Log.d("OBjeto lavadora", lavadora.toString());
+        //Creando una segunda instancia de nuestro objeto
+        Lavadora lavadoraSecadora = new Lavadora("IEM", "Secado Automático");
+        Log.d("Objeto lavadora-Secadora", lavadoraSecadora.toString());
+
+        //Realizar que los botones muestren en el TextView, los valores que tienen los objetos
+
+        //Instanciar los objetos de nuestro XML
+        TextView textView = (TextView) findViewById(R.id.tvMensaje);
+        Button btnObjeto1 = (Button) findViewById(R.id.btnObjeto1);
+        Button btnObjeto2 = (Button) findViewById(R.id.btnObjeto2);
+
+        btnObjeto1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(lavadora.toString());
+            }
+        });
+
+        btnObjeto2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(lavadoraSecadora.toString());
+            }
+        });
+
 
     }
+
+
 }
