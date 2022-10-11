0df89170a8241cfa746edd5f5c846f5868973000
Edgar D; Caamal <caamal.edgar@gmail.com>
2022-10-11 11:47:45 -0500

    Manejo de im{agen Glide, Dummy de Red Social

--- app/src/main/java/com/example/objetos/MainActivity.java
+++ app/src/main/java/com/example/objetos/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.objetos.Clases.Lavadora;
 
 import java.util.Random;
@@ -27,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String urlLogoPrincipal = "https://oasishoteles.com/blog/wp-content/uploads/2021/03/centro-campeche.jpg";
+        ImageView ivLogo1 = (ImageView) findViewById(R.id.ivlogo1);
+        ImageView ivLogo2 = (ImageView) findViewById(R.id.ivlogo2);
+
+
+
         //Creando una instancia de nuestro objeto
         Lavadora lavadora   = new Lavadora();
         lavadora.setMarca("Mabe");
@@ -43,6 +50,23 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.tvMensaje);
         Button btnObjeto1 = (Button) findViewById(R.id.btnObjeto1);
         Button btnObjeto2 = (Button) findViewById(R.id.btnObjeto2);
+
+
+        Glide.with(this)
+                .load(urlLogoPrincipal)
+                .error(getApplicationContext().getDrawable(android.R.drawable.presence_offline))
+                .fitCenter()
+                .circleCrop()
+                .into(ivLogo1);
+
+        urlLogoPrincipal = "https://blog.xcaret.com/es/wp-content/uploads/2014/06/portada-Blogs-1080x640.jpg";
+        Glide.with(this)
+                .load(urlLogoPrincipal)
+                .error(getApplicationContext().getDrawable(android.R.drawable.presence_offline))
+                .fitCenter()
+                .circleCrop()
+                .into(ivLogo2);
+
 
         btnObjeto1.setOnClickListener(new View.OnClickListener() {
             @Override
